076b77689f48300482c9da72c69dacb7c9e713ce
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-09-15 21:59:39 +0300

    add the product service

---
+++ productservice/src/test/java/com/micro/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
