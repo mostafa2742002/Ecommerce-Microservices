963e0367c2a89dda4c7da3dd6bb82d07c807cc09
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-09-19 15:54:07 +0300

    Make All Entity And DTO Consistent In All Microservices

--- inventory/src/main/java/com/micro/inventory/entity/Cart.java
+++ inventory/src/main/java/com/micro/inventory/entity/Cart.java
@@ -1,47 +1,39 @@
 package com.micro.inventory.entity;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.micro.inventory.dto.ProductDTO;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
 @Entity
-@NoArgsConstructor
-@Table(name = "products")
-public class Product {
-
+@Data
+@Table(name = "carts")
+public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private String name;
-
-    private String description;
-
-    private BigDecimal price;
-
-
     @ManyToOne
-    @JoinColumn(name = "category_id")
+    @JoinColumn(name = "user_id")
     @JsonBackReference // Prevent infinite recursion
-    private Category category;
+    private User user;
 
-    public Product(ProductDTO productDTO, Category category) {
-        this.name = productDTO.getName();
-        this.description = productDTO.getDescription();
-        this.price = productDTO.getPrice();
-        this.category = category;
-    }
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
 
+    private BigDecimal totalPrice;
+
+    // Getters and setters
 }
---
+++ inventory/src/main/java/com/micro/inventory/entity/CartItem.java
@@ -0,0 +1,29 @@
+package com.micro.inventory.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "cart_items")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+    private Long quantity;
+    private BigDecimal price;
+
+    // Getters and setters
+}
