1774bc429a88876f0d82c2df9bdf579a8a30f2cd
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-09-25 13:31:00 +0300

    Init The Eureka Service

---
+++ eurekaservice/src/main/java/com/micro/eurekaservice/EurekaserviceApplication.java
@@ -0,0 +1,13 @@
+package com.micro.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserviceApplication.class, args);
+	}
+
+}
---
+++ eurekaservice/src/test/java/com/micro/eurekaservice/EurekaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
