b6b90bdba14e6545149c04c75128258678e353af
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-09-28 17:47:56 +0300

    Add route configuration for e-commerce services in Gateway Service
    The code changes add a new route configuration for the e-commerce services in the Gateway Service. This allows the Gateway Service to route requests to the appropriate microservices based on the specified paths. The routes are defined for the cartservice, inventoryservice, orderservice, paymentservice, productservice, and userservice.

--- gatewayservice/src/main/java/com/micro/gatewayservice/GatewayserviceApplication.java
+++ gatewayservice/src/main/java/com/micro/gatewayservice/GatewayserviceApplication.java
@@ -2,12 +2,47 @@ package com.micro.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserviceApplication.class, args);
+	}
+
+	// the services we have is cartservice, inventoryservice, orderservice, paymentservice, productservice, userservice
+
+	@Bean
+	public RouteLocator ecommerceRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+					.path("/e-commerce/cartservice/**")
+					.filters(f -> f.rewritePath("/e-commerce/cartservice/(?<segment>.*)", "/${segment}"))
+					.uri("lb://cartservice"))
+				.route(p -> p
+					.path("/e-commerce/inventoryservice/**")
+					.filters(f -> f.rewritePath("/e-commerce/inventoryservice/(?<segment>.*)", "/${segment}"))
+					.uri("lb://inventoryservice"))
+				.route(p -> p 
+					.path("/e-commerce/orderservice/**")
+					.filters(f -> f.rewritePath("/e-commerce/orderservice/(?<segment>.*)", "/${segment}"))
+					.uri("lb://orderservice"))
+				.route(p -> p
+					.path("/e-commerce/paymentservice/**")
+					.filters(f -> f.rewritePath("/e-commerce/paymentservice/(?<segment>.*)", "/${segment}"))
+					.uri("lb://paymentservice"))
+				.route(p -> p
+					.path("/e-commerce/productservice/**")
+					.filters(f -> f.rewritePath("/e-commerce/productservice/(?<segment>.*)", "/${segment}"))
+					.uri("lb://productservice"))
+				.route(p -> p
+					.path("/e-commerce/userservice/**")
+					.filters(f -> f.rewritePath("/e-commerce/userservice/(?<segment>.*)", "/${segment}"))
+					.uri("lb://userservice"))
+				.build();
 	}
 
 }
