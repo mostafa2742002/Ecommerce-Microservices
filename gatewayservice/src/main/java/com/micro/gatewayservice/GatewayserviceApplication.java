70c4a79a2bd20ee1e39d77faf6e57d9b147ee5ba
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-09-28 14:17:50 +0300

    Init The Gateway Service

---
+++ gatewayservice/src/main/java/com/micro/gatewayservice/GatewayserviceApplication.java
@@ -0,0 +1,13 @@
+package com.micro.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserviceApplication.class, args);
+	}
+
+}
